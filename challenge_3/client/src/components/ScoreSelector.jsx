2bd32578d9f496f7629a9b0d34c99597c8f87683
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-03-25 12:58:55 -0700

    Saves progress

--- challenge_3/client/src/components/ScoreSelector.jsx
+++ challenge_3/client/src/components/ScoreSelector.jsx
@@ -8,15 +8,18 @@ class ScoreSelector extends React.Component {
       prevScore: 0,
       totalScore: 0,
       bowls: 0,
-      round: 0,
+      round: 1,
       spare: false,
       strike: false,
+      score1: 0,
+      score2: 0,
     }
     this.submitScore = this.submitScore.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSpare = this.handleSpare.bind(this);
-    this.handleStrike = this.handleStrike.bind(this);
-    this.handleStrikeScore = this.handleStrikeScore.bind(this);
+    // this.handleStrike = this.handleStrike.bind(this);
+    // this.handleStrikeScore = this.handleStrikeScore.bind(this);
+    this.submitBowl = this.submitBowl.bind(this);
   }
 
   handleChange(event) {
@@ -25,32 +28,67 @@ class ScoreSelector extends React.Component {
     this.setState(newState);
   }
 
+  // handleSpare() {
+  //   this.setState({
+  //     spare: (this.state.bowls % 2 === 1) && (parseInt(this.state.score) + this.state.prevScore === 10) ? true : false,
+  //   })
+  // }
+
   handleSpare() {
     this.setState({
-      spare: (this.state.bowls % 2 === 1) && (parseInt(this.state.score) + this.state.prevScore === 10) ? true : false,
+      spare: (parseInt(this.state.score1) + parseInt(this.state.score2)) === 10 ? true : false,
     })
   }
 
-  handleStrike() {
-    this.setState({
-      strike: (this.state.bowls % 2 === 0) && (parseInt(this.state.score) === 10) ? true : false,
-    })
-  }
 
-  handleStrikeScore() {
-    
+  // handleStrike() {
+  //   this.setState({
+  //     strike: (parseInt(this.state.score1) === 10) ? true : false,
+  //   })
+  // }
+
+  // handleStrikeScore() {
+  //   if (this.state.score === 10) {
+  //     this.setState({
+  //       bowls: parseInt(this.state.bowls) + 2,
+  //       round: parseInt(this.state.round) + 1,
+
+  //     })
+  //   }
+  // }
+
+  submitBowl() {
+    this.handleSpare();
+    // this.handleStrike();
+    if (this.state.bowls % 2 === 0 && parseInt(this.state.score) === 10) {
+      this.setState({
+        bowls: this.state.bowls + 2,
+        score1: parseInt(this.state.score),
+        strike: true,
+      })
+    } else if (this.state.bowls % 2 === 0) {
+      this.setState({
+        bowls: this.state.bowls + 1,
+        // round: this.state.round + 1,
+        score1: parseInt(this.state.score),
+      })
+    } else {
+      this.setState({
+        bowls: this.state.bowls + 1,
+        score2: parseInt(this.state.score),
+      })
+    }
   }
 
   submitScore() {
-    this.handleSpare();
-    this.handleStrike();
+    // this.handleStrike();
     this.setState({
       prevScore: parseInt(this.state.score),
-      totalScore: this.state.spare ? (parseInt(this.state.score) * 2 + this.state.totalScore) :
-      this.state.strike ? (parseInt(this.state.score) * 2 + (this.state.prevScore * 2) + this.state.totalScore) :
-      parseInt(this.state.score) + parseInt(this.state.totalScore),
-      bowls: parseInt(this.state.bowls) + 1,
-      round: this.state.bowls % 2 === 1 ? this.state.round + 1 : this.state.round,
+      totalScore: this.state.spare ? ((parseInt(this.state.score1) * 2) + parseInt(this.state.score2) + this.state.totalScore) :
+      this.state.strike ? ((parseInt(this.state.score1) * 2) + (parseInt(this.state.score2 * 2)) + this.state.totalScore) :
+      parseInt(this.state.score1) + parseInt(this.state.score2) + parseInt(this.state.totalScore),
+      // bowls: this.state.strike ? this.state.bowls + 2 : this.state.bowls,
+      round: parseInt(this.state.round) + 1
     })
   }
 
@@ -71,6 +109,7 @@ class ScoreSelector extends React.Component {
           <option value="9">9</option>
           <option value="10">10</option>
         </select>
+        <button onClick={this.submitBowl}>SUBMIT BOWL</button>
         <button onClick={this.submitScore}>SUBMIT SCORE</button>
         <div>
           TOTAL SCORE:  
@@ -86,6 +125,16 @@ class ScoreSelector extends React.Component {
           TOTAL BOWLS:  
           {'  '}
           {this.state.bowls}
+        </div>
+        <div>
+          SCORE 1:  
+          {'  '}
+          {this.state.score1}
+        </div>
+        <div>
+          SCORE 2:  
+          {'  '}
+          {this.state.score2}
         </div>
       </div>
     )
