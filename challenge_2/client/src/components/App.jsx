8a227aa987ffa6623ccf8818d3539acf8b18ba08
Ryan Cowie <ryancowie@Ryans-Pro.local>
2019-03-25 20:58:35 -0700

    Retrieves data from the API

--- challenge_2/client/src/components/App.jsx
+++ challenge_2/client/src/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data: [],
     }
   }
+  
+  componentDidMount() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then((res) => {
+        this.setState({
+          data: res.data.bpi
+        })
+      })
+  }
+
   render() {
+    console.log(this.state.data)
     return (
       <div>
         Hello
